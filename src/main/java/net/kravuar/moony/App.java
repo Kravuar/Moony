9639ade2e7982d3a6bb1e282adb4ab9727341d58
Kravuar <steam_webmoney@mail.ru>
2022-06-22 14:37:20 +0300

    damn it

--- src/main/java/net/kravuar/moony/App.java
+++ src/main/java/net/kravuar/moony/App.java
@@ -19,8 +19,7 @@ public class App extends Application {
         try {
             URL url = AppLauncher.class.getProtectionDomain().getCodeSource().getLocation();
             File myFile = new File(url.toURI());
-            //ExecutablePath = myFile.getParentFile().getPath();
-            ExecutablePath = "E:\\Balovstvo\\moony";
+            ExecutablePath = myFile.getParentFile().getPath();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
